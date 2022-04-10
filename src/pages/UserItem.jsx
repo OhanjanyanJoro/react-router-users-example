683133fc1f34991a88a87cf21ad89b51ea2545b3
Antonyan7 <antonyansergey2002@gmail.com>
2022-04-10 13:58:04 +0400

    poqr popoxutyun

--- src/pages/UserItem.jsx
+++ src/pages/UserItem.jsx
@@ -35,7 +35,7 @@ export default function UsersItem(){
             !isLaoding && (
                     user ? (
                     <h1>user ID: {user.id},  user Name: {user.name}</h1>
-                ) : 'user Not Found'
+                ) : 'User Not Found'
             )
         }
     </div>
